20d5ce017931327ad2f485498c2c650e8ec081b3
ifazzzz <shoaibahmedifaz@gmail.com>
2022-11-24 11:03:28 +0600

    folders added

--- src/configurations/firebase.config.js
+++ src/configurations/firebase.config.js
@@ -1,16 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// TODO: 
+// https:/
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAG1NQ18BvkhYy4YesPM5rBMI9Puc0Khso",
-  authDomain: "readers-port.firebaseapp.com",
-  projectId: "readers-port",
-  storageBucket: "readers-port.appspot.com",
-  messagingSenderId: "787458410303",
-  appId: "1:787458410303:web:a6e9041c23d91f6815758a"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
